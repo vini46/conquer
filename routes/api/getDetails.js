9ef83383a2ec8866f0d0b749c28c6ca5eaae242a
vinod <vinod.baradwaj@live.com>
2020-04-19 19:03:34 +0530

    cors added

--- routes/api/getDetails.js
+++ routes/api/getDetails.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const sheets = require('../../service/sheets');
 
-router.get('/', async (req, res) => {
+router.get('/', cors(), async (req, res) => {
     const data = await sheets.getAllData();
     const jsonData = sheets.constructJson(data);
     res.status(200).contentType('application/json').json(jsonData);
 })
 
-router.post('/', async (req, res) => {
+router.post('/', cors(), async (req, res) => {
     sheets.addData(req.body);
     res.status(201).json({msg: 'ok'});
 })
